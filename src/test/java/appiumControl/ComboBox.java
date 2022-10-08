2b6a13d103d08579e27f3e773f88cdec1005c8e2
Beto <72410982+TheFnX@users.noreply.github.com>
2022-10-08 00:07:29 -0400

    Add files via upload

---
+++ src/test/java/appiumControl/ComboBox.java
@@ -0,0 +1,13 @@
+package appiumControl;
+
+import org.openqa.selenium.By;
+
+public class ComboBox  extends AppiumControl{
+    public ComboBox(By locator){
+        super(locator);
+    }
+
+    public void select(String value){
+
+    }
+}
---
+++ src/test/java/appiumControl/RadioButton.java
@@ -0,0 +1,10 @@
+package appiumControl;
+
+import org.openqa.selenium.By;
+
+public class RadioButton extends AppiumControl{
+
+    public RadioButton(By locator){
+        super(locator);
+    }
+}
